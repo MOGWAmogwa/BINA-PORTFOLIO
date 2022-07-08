e25d63c7f2818dd9c26fbdc78a89e60a033f335a
MOGWAmogwa <hongbin9696@gmail.com>
2022-07-08 09:27:38 +0900

    JS 카테고리 버튼 누르면 해당 섹션으로 이동

---
+++ script.js
@@ -0,0 +1,7 @@
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e)=>{
+    const target = e.target;
+    const link = target.dataset.link;
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior : 'smooth'});
+})
